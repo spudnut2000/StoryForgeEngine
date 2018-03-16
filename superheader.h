521d24de421f1e311284ed8760292ab26ebab9b5
trevthedev <hilliblockmrblock@gmail.com>
2018-03-16 00:54:02 +0000

    Added superheader

---
+++ superheader.h
@@ -0,0 +1,15 @@
+fndef SUPERHEADER_H_INCLUDED
+#define SUPERHEADER_H_INCLUDED
+
+#include <GLFW/glfw3>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <iostream>
+#include <math.h>
+#include <string.h>
+#include <windows.h>
+#include <windowsx.h>
+#include <fstream>
+
+
+#endif // SUPERHEADER_H_INCLUDED
